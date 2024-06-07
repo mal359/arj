1a55e9de45bd87779e5cf14413a89fbd2d088725
Matthew A. Lukowicz <mlukowicz@sdf.org>
2024-06-07 01:34:44 -0400

    Add macports patches.

--- uxspec.c
+++ uxspec.c
@@ -397,14 +397,14 @@ int set_owner(char FAR *storage, char *name, int resolve)
       group received from getpwnam() */
    if(i<l-1&&(gr=getgrnam(tmp+i+1))!=NULL)
     gid=gr->gr_gid;
-   rc=lchown(name, pw->pw_uid, gid);
+   rc=chown(name, pw->pw_uid, gid);
    return(rc);
   }
   else
   {
    if(l!=8)
     return(-1);
-   return(lchown(name, mget_dword(storage+1), mget_dword(storage+5)));
+   return(chown(name, mget_dword(storage+1), mget_dword(storage+5)));
   }
  #else
   return(-1);

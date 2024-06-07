785554d952b36ad0a66e24b8687c31deeea7a703
Matthew A. Lukowicz <mlukowicz@sdf.org>
2024-06-07 01:21:46 -0400

    Add FreeBSD patch.
    Fix BUILD when work is under a NFS mounted partition
    https://cgit.freebsd.org/ports/commit/archivers/arj/files?id=9b5ac4db87e004b2f2881a86a0ca578b100d5ded

--- environ.c
+++ environ.c
@@ -2064,7 +2064,7 @@ FILE *file_open(char *name, char *mode)
       /* ASR fix 01/10/2003 -- re-fix to handle umask 022 correctly */
       if((handle=open(name, oflag, 0644))==-1)
        return(NULL);
-      if(fcntl(handle, F_SETLK, &flk)==-1&&errno!=EINVAL)
+      if(fcntl(handle, F_SETLK, &flk)==-1&&errno!=EINVAL&&errno!=ENOTSUP)
       {
        close(handle);
        return(NULL);
